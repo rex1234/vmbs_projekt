6d3e54ea04532763a1983476c1f89ccaa4a3d3ac
Jan Mjartan <mjartan.jan@gmail.com>
2015-04-21 12:18:06 +0200

    create board form

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,8 @@ package controllers;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import models.Board;
+import models.Member;
+import play.data.DynamicForm;
 import play.libs.F;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -11,19 +13,37 @@ import services.MyWebSocketActor;
 import services.RestService;
 import views.html.index;
 
+import static play.data.Form.form;
+
 public class Application extends Controller {
 
     public static Result index() {
-        for (Board board : RestService.getInstance().getBoards()) {
-            RestService.getInstance().deleteBoard(board.getId());
-        }
         return ok(index.render("Heeeej nasa mega apliacia"));
     }
+
     public static WebSocket<String> socket() {
         return WebSocket.withActor(new F.Function<ActorRef, Props>() {
             public Props apply(ActorRef out) throws Throwable {
                 return MyWebSocketActor.props(out);
             }
         });
+    }
+
+    public static Result board(String id) {
+        return play.mvc.Results.TODO;
+    }
+
+    public static Result newBoard() {
+        DynamicForm dynamicForm = form().bindFromRequest();
+        Board board = new Board();
+        board.setName(dynamicForm.get("boardName"));
+
+        Member member = new Member();
+        member.setName(dynamicForm.get("userName"));
+
+        RestService.getInstance().createBoard(board);
+        RestService.getInstance().createMember(board.getId(), member);
+
+        return redirect("/board/" + board.getId());
     }
 }
